75b5160425f3755cea27f449eda1a08ecffacf08
Vidya Mani <mani.vidya@gmail.com>
2012-08-09 16:30:13 -0700

    Replaced d3 with simpler highcharts

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,8 +13,11 @@ class TweetsController < ApplicationController
   end
   
   def charts
-    @neutral_count = Tweet.where(:sentiment => "neutral").count
-    @positive_count = Tweet.where(:sentiment => "positive").count
-    @negative_count = Tweet.where(:sentiment => "negative").count
+    neutral_count = Tweet.where(:sentiment => "neutral").count
+    positive_count = Tweet.where(:sentiment => "positive").count
+    negative_count = Tweet.where(:sentiment => "negative").count
+    @pie_chart_data = "[{name: 'Positive', y: #{positive_count}, color: '#228B22'},
+                       {name: 'Negative', y: #{negative_count}, color: '#FF3030'},
+                       {name: 'Neutral', y: #{neutral_count}, color: '#97FFFF'}]"
   end
 end
