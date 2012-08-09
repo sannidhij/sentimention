d1f5660a6e9f09ebe5b2651f510fd58673b3e524
Vidya Mani <mani.vidya@gmail.com>
2012-08-09 12:10:08 -0700

    Added mock call to sentiment api to avoid callback.

--- spec/tweet_spec.rb
+++ spec/tweet_spec.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe Tweet do
   describe "create" do
     it "should save the tweet with original content" do
-      Tweet.create(original: "foo").should_not be_nil
+      Twitter.should_receive(:current_sentiment).and_return(:positive)
+      Tweet.create(original: {name: 'bob'}).should_not be_nil
     end
   end
 end
