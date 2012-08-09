c8c610f01f0ee1d64bb53241b29613e0707613d1
Vidya Mani <mani.vidya@gmail.com>
2012-08-09 10:41:37 -0700

    Changed field type for original to Hash for easier access.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet
   include Mongoid::Document
-  field :original, type: String
+  field :original, type: Hash
+  field :sentiment, type: String
+  
+  before_create :fetch_sentiment
+
+  def fetch_sentiment
+    # p original['text'], Twitter.current_sentiment(original['text'])
+  end
 end
