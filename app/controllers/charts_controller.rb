d36b8bec0680eaa6d17ee031ae9451e0a90bb092
Sheroy Marker <sheroy@gmail.com>
2012-08-10 14:28:22 -0700

    Calling map reduce to generate user aggregates before rendering the tweets per user chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -26,6 +26,7 @@ class ChartsController < ApplicationController
   end
   
   def top_tweeters_chart
+    generate_user_aggregates
     user_criteria = UserAggregates.desc('value.total_tweets').limit(10)
     @top_tweet_user_names = user_criteria.map { |ua| ua['_id']['user_name'] }
     total_trend = user_criteria.map { |ua| ua['value']['total_tweets'] }
@@ -36,6 +37,47 @@ class ChartsController < ApplicationController
                           {name: 'Neutral', data: #{neutral_trend}, color: '#97FFFF'}, 
                           {name: 'Positive', data: #{positive_trend}, color: '#228B22'},
                           {name: 'Total', data: #{total_trend} }]"
+  end
+
+  private
+  def generate_user_aggregates
+    map = %Q{
+      function () {
+      	var positive_tweets = 0, negative_tweets = 0, neutral_tweets = 0;
+      	switch (this.sentiment) { 
+      		case 'positive':
+      			positive_tweets = 1;
+      			break;
+      		case 'negative':
+      			negative_tweets = 1;
+      			break;
+      		case 'neutral':
+      			neutral_tweets = 1;
+      			break;
+      	} 
+
+      	emit({user_name:this.original.from_user_name }, { total_tweets:1, positive_tweets:positive_tweets, negative_tweets:negative_tweets, neutral_tweets:neutral_tweets, sentiment:this.sentiment });
+      }
+    }
+    
+    reduce = %Q{
+      function(key, values) {
+      		 var total_tweets = 0, negative_tweets = 0, neutral_tweets = 0;
+      		 var positive_tweets = 0; 
+      		 values.forEach(function(v) {
+      		 									total_tweets += v['total_tweets'];
+      		 									if (v['sentiment'] === 'positive') { positive_tweets += v['positive_tweets']; };
+      											if (v['sentiment'] === 'negative') { negative_tweets += v['negative_tweets']; };
+      											if (v['sentiment'] === 'neutral') { neutral_tweets += v['neutral_tweets']; };
+      											sentiment = v['sentiment'];
+      											});
+      		return {total_tweets: total_tweets, positive_tweets: positive_tweets, negative_tweets: negative_tweets, neutral_tweets: neutral_tweets};
+      }
+    }
+    
+    Tweet.map_reduce(map, reduce).out(merge: "user_aggregates").each do |document|
+      p document
+    end
   end
   
 end
