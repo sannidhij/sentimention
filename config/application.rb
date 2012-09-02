726589c9dabbb7708faeee50acfa61245adf7d77
Sheroy Marker <sheroy@gmail.com>
2012-09-02 08:54:58 -0700

    Specifyig ruby version in the Gemfile

--- config/application.rb
+++ config/application.rb
@@ -60,9 +60,9 @@ module Sentimention
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.autoload_paths += Dir["#{config.root}/app/models/**/"]
   end
 end
